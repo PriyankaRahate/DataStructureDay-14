b3e7e776d60da49919ee0442ce164eb6ea1c8b52
unknown <priyanka.rahate23@gmail.com>
2022-04-06 15:40:45 +0530

    CreateLinkList Program

---
+++ src/datastrucDay14/CreateLinkListUC1.java
@@ -0,0 +1,31 @@
+package datastrucDay14;
+
+public class CreateLinkListUC1 {
+
+    Node head;
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+    	CreateLinkListUC1  linkedList = new CreateLinkListUC1 ();
+
+        linkedList.head = new Node(56);
+        Node second = new Node(30);
+        Node Third = new Node(70);
+
+        linkedList.head.next = second;
+        second.next = Third;
+
+        // Print the Data with Values
+        while (linkedList.head != null) {
+            System.out.println(linkedList.head.data);
+            linkedList.head = linkedList.head.next;
+        }
+    }
+}
