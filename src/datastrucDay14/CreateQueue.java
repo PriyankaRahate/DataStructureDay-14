a8b6f9a775e9ed6448d622737336827019a93a0c
unknown <priyanka.rahate23@gmail.com>
2022-04-06 17:40:12 +0530

    CreateQueue Program

---
+++ src/datastrucDay14/CreateQueue.java
@@ -0,0 +1,52 @@
+
+package datastrucDay14;
+
+	public class CreateQueue{
+		 public Node head;
+		    public Node tail;
+		    static class Node {
+		        int data;
+		        Node next;
+
+		        Node(int d) {
+		            data = d;
+		            next = null;
+		        }
+		    }
+
+		    // Append datas One By One
+		    public void append(int datas){
+		        Node newNode = new Node(datas);
+		        if(head == null){
+		            head = newNode;
+		            tail = newNode;
+		        }
+		        else {
+		            tail.next = newNode;
+		            tail = newNode;
+		        }
+		    }
+
+		    // Display all the Node with value
+		    public void display() {
+		        Node current = head;
+		        if(head == null) {
+		            System.out.println("List is empty");
+		        }
+		        while (current != null) {
+		            System.out.println(current.data +" ");
+		            current = current.next;
+		        }
+		    }
+
+		    public static void main(String[] args) {
+		    	CreateQueue operQueue = new CreateQueue();
+		        operQueue.append(70);
+		        operQueue.append(30);
+		        operQueue.append(56);
+
+		        operQueue.display();
+		    }
+
+		
+	}
